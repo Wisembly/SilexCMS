4062c25250e4d2de18705e46012d8aa9367b1460
Maël Nison <nison.mael@gmail.com>
2012-03-26 15:39:08 +0200

    Adds core classes

---
+++ src/SilexCMS/Core/AbstractRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace SilexCMS\Core;
+
+abstract class AbstractRepository
+{
+    protected $table = null;
+	
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+	
+    public function query($query, $arguments = array())
+    {
+        return $this->db->executeQuery($query, $arguments);
+    }
+	
+    public function execute($query, $arguments = array())
+    {
+        return $this->db->executeUpdate($query, $arguments);
+    }
+	
+    public function select($condition)
+    {
+        if (is_numeric($condition)) {
+            $condition = array('id' => $condition);
+        }
+        
+        $where = implode(' AND ', array_map(function ($name) { return $name . ' = ?'; }, array_keys($condition)));
+		
+        if (!empty($where)) {
+            $where = ' WHERE ' . $where;
+        }
+        
+        return $this->db->executeQuery("SELECT * FROM {$this->table}{$where}", array_values($condition));
+    }
+	
+    public function insert($values)
+    {
+        return $this->db->insert($this->table, $values);
+    }
+	
+    public function update($condition, $values)
+    {
+        if (is_numeric($condition)) {
+            $condition = array('id' => $condition);
+        }
+        
+        return $this->db->update($this->table, $values, $condition);
+    }
+	
+    public function delete($condition)
+    {
+        if (is_numeric($condition)) {
+            $condition = array('id' => $condition);
+        }
+        
+        return $this->db->delete($this->table, $condition);
+    }
+	
+    public function findAll()
+    {
+        return $this->db->fetchAll("SELECT * FROM {$this->table}");
+    }
+}
---
+++ src/SilexCMS/Core/TransientResponse.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace SilexCMS\Core;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class TransientResponse extends Response
+{
+    private $twig;
+    
+    private $template;
+	
+    private $variables;
+	
+    public function __construct($twig, $template, $variables = null)
+    {
+        $this->template = $twig->loadTemplate($template);
+		
+        $this->variables = (object) $variables;
+		
+        parent::__construct();
+    }
+	
+    public function getTemplate()
+    {
+        return $this->template;
+    }
+	
+    public function getVariables()
+    {
+        return $this->variables;
+    }
+	
+    public function prepare($req)
+    {
+        $this->setContent($this->template->render((array) $this->variables));
+		
+        parent::prepare($req);
+    }
+}
