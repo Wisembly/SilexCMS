8383fd5dae26e0587c9e1e7cc4ba21f43d2d21ff
Guillaume Potier <cobalt2760@gmail.com>
2012-09-03 17:03:12 +0200

    - reverted previous commit. Added foreign function in twig to retrieve foreign id in a dataset

--- src/SilexCMS/Repository/AbstractRepository.php
+++ src/SilexCMS/Repository/AbstractRepository.php
@@ -48,24 +48,7 @@ abstract class AbstractRepository extends DataMap
             $where = ' WHERE ' . $where;
         }
 
-        return $this->reIndex($this->db->executeQuery("SELECT * FROM {$this->table}{$where}", array_values($condition))->fetchAll());
-    }
-
-    private function reIndex($results)
-    {
-        $indexedArray = array();
-
-        // reindex array only if we have an id field an not a single element
-        if (!isset($results[0]['id']) || sizeof($results) == 1) {
-            return $results;
-        }
-
-        foreach ($results as $result)
-        {
-            $indexedArray[$result['id']] = $result;
-        }
-
-        return $indexedArray;
+        return $this->db->executeQuery("SELECT * FROM {$this->table}{$where}", array_values($condition))->fetchAll();
     }
 
     public function insert($values)
---
+++ src/SilexCMS/Twig/Extension/ForeignKeyExtension.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace SilexCMS\Twig\Extension;
+
+use Silex\Application;
+
+class ForeignKeyExtension extends \Twig_Extension
+{
+    private $app;
+
+    function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    public function getName() {
+        return "SilexCMS_ForeignKey";
+    }
+
+    public function getFunctions()
+    {
+        return array(
+            "foreign"        => new \Twig_Function_Method($this, "foreign"),
+        );
+    }
+
+    public function foreign($dataset, $id)
+    {
+        if (!isset($dataset[0]['id'])) {
+            return false;
+        }
+
+        foreach ($dataset as $data) {
+            if ($data['id'] == $id) {
+                return $data;
+            }
+        }
+
+        return false;
+    }
+}
